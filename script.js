7ebba502d25264639b2c9bad1dd5e8693bcf17af
Rami <rami.elsaneh@protonmail.com>
2025-09-30 14:45:28 +0200

    added a view more and view less logic in js

--- script.js
+++ script.js
@@ -3,5 +3,12 @@ const viewMoreBtn = document.getElementById("view-more-btn");
 const hiddenArticle = document.getElementById("hidden-article")
 
 viewMoreBtn.addEventListener("click", function() {
-	
+ if (hiddenArticle.style.display === "none" || hiddenArticle.style.display === "") {
+    hiddenArticle.style.display = "block";
+    viewMoreBtn.textContent = "Show Less";  // Byt knapptext till "Show Less"
+  } else {
+    // Om artikeln är synlig, dölj den
+    hiddenArticle.style.display = "none";
+    viewMoreBtn.textContent = "View More";  // Återställ knapptexten till "View More"
+  }
 });
